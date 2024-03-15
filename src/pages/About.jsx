0709d013b9e863d227e2ffc8698b3986a93ee2d4
Ram-12212 <163520393+Ram-12212@users.noreply.github.com>
2024-03-15 16:46:31 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,12 +4,12 @@ const About = () => {
     <>
       <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
         <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl '>
-          We love
+          We ❤️
         </h1>
         <div className='stats bg-primary shadow'>
           <div className='stat'>
             <div className='stat-title text-primary-content text-4xl font-bold tracking-widest'>
-              comfy
+              Shopping
             </div>
           </div>
         </div>
